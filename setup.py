14a9709ef6325c73615d2509b638a0f7d59da3f5
1362136kk <krishna.kalathur@gmail.com>
2023-08-12 22:32:21 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+  
+with open("README.md", "r") as fh:
+    description = fh.read()
+  
+setuptools.setup(
+    name="test-package",
+    version="0.0.1",
+    author="Krishna Kalathur",
+    author_email="krishna.kalathur@gmail.com",
+    packages=["test_package"],
+    description="A sample test package",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/1362136/SPO-Plus-Training-Package",
+    license='MIT',
+    python_requires='>=3.8',
+    install_requires=[]
+)
